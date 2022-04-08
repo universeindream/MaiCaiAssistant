68e3cbd7ee91d6d617ad9d4b1d9c81eec01aa368
itwangxiang <itwangxiang@foxmail.com>
2022-04-08 11:24:20 +0800

    chore: rename

--- app/src/androidTest/java/com/univerindream/mallhelp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/univerindream/mallhelp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.univerindream.mallhelp", appContext.packageName)
+        assertEquals("com.univerindream.app", appContext.packageName)
     }
 }
