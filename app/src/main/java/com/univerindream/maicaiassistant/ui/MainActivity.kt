5bb7a4e0342d04f445e17fb8bdbf4e3b03737a2c
itwangxiang <itwangxiang@foxmail.com>
2022-04-12 09:48:09 +0800

    feat: 版本更新时，提示方案可能更新

--- app/src/main/java/com/univerindream/maicaiassistant/ui/MainActivity.kt
+++ app/src/main/java/com/univerindream/maicaiassistant/ui/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.univerindream.maicaiassistant.ui
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.blankj.utilcode.util.SPUtils
+import com.univerindream.maicaiassistant.BuildConfig
+import com.univerindream.maicaiassistant.MHData
 import com.univerindream.maicaiassistant.R
 import com.univerindream.maicaiassistant.databinding.ActivityMainBinding
 
@@ -25,11 +29,30 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_settings)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        checkVersion()
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_settings)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    fun checkVersion() {
+        val versionCode = SPUtils.getInstance().getInt("versionCode", 0)
+        if (BuildConfig.VERSION_CODE > versionCode) {
+            SPUtils.getInstance().put("versionCode", BuildConfig.VERSION_CODE)
+
+            AlertDialog.Builder(this)
+                .setTitle("方案更新提示")
+                .setMessage("方案可能有更新，是否重置本地方案")
+                .setNegativeButton("取消") { _, _ -> }
+                .setPositiveButton("确定") { _, _ ->
+                    MHData.curJsonMHSolution = ""
+                    MHData.allJsonMHSolution = ""
+                }
+                .show()
+        }
     }
 }
