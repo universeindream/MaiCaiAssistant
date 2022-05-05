bbce1527f63b8a17cd902b9266e94cdcd178e566
itwangxiang <itwangxiang@foxmail.com>
2022-05-05 10:43:53 +0800

    fix: build

--- app/src/main/java/com/univerindream/maicaiassistant/MCApp.kt
+++ app/src/main/java/com/univerindream/maicaiassistant/MCApp.kt
@@ -1,25 +1,17 @@
 package com.univerindream.maicaiassistant
 
 import android.app.Application
-import androidx.work.Configuration
 import cat.ereza.customactivityoncrash.config.CaocConfig
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.SupervisorJob
 
 
 @HiltAndroidApp
-class MCApp : Application(), Configuration.Provider {
+class MCApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
         CaocConfig.Builder.create().apply()
     }
-
-    override fun getWorkManagerConfiguration(): Configuration =
-        Configuration.Builder()
-            .setMinimumLoggingLevel(if (BuildConfig.DEBUG) android.util.Log.DEBUG else android.util.Log.ERROR)
-            .build()
 
 }
